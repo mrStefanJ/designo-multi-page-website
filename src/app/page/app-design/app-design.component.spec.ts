53f6c40c132b27530f2976cfd7adab82bfe8a879
Stefan <stefan.joncic@united.group>
2022-05-18 16:23:13 +0200

    new pages

---
+++ src/app/page/app-design/app-design.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AppDesignComponent } from './app-design.component';
+
+describe('AppDesignComponent', () => {
+  let component: AppDesignComponent;
+  let fixture: ComponentFixture<AppDesignComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AppDesignComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppDesignComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
