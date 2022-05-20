be9b56c07a2634b4e8f5f1b90a9f7ac0eb6de6cd
Stefan <stefan.joncic@united.group>
2022-05-20 16:46:21 +0200

    app-design page styling

--- src/app/page/app-design/app-design.component.ts
+++ src/app/page/app-design/app-design.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/service/api.service';
 
 @Component({
   selector: 'app-app-design',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppDesignComponent implements OnInit {
 
-  constructor() { }
+  dataApiDesign: any;
+  design: any;
+
+  constructor(private service: ApiService) { }
 
   ngOnInit(): void {
+    this.getData();
+    this.getDataDesign();
   }
 
+  getData(){
+    this.service.getDataAppDesign().subscribe(res => {
+      this.dataApiDesign = res;
+    })
+  }
+
+  getDataDesign(){
+    this.service.getDataDesign().subscribe(res => {
+      this.design = res;
+    })
+  }
 }
