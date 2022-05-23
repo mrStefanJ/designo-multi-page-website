ffd8d71da3cdb783f72887c63da4d595b64ea3af
Stefan <stefan.joncic@united.group>
2022-05-23 12:55:38 +0200

    aout-us page, styling

--- src/app/page/about-us/about-us.component.ts
+++ src/app/page/about-us/about-us.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutUsComponent implements OnInit {
 
+  imageHero: string = '../../../assets/images/about/desktop/image-about-hero.jpg';
+  imageRealDeal: string = '../../../assets/images/about/desktop/image-real-deal.jpg';
+  imageWorlClass: string = '../../../assets/images/about/desktop/image-world-class-talent.jpg';
+
+  imgCAN: string = '../../../assets/images/contact/illustration-canada.svg';
+  imgAUS: string = '../../../assets/images/contact/illustration-australia.svg';
+  imgUK: string = '../../../assets/images/contact/illustration-united-kingdom.svg';
+
   constructor() { }
 
   ngOnInit(): void {
