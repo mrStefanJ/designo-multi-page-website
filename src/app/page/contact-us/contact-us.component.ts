16db9d2456a3998d1ee3998a597fa6752bc66b02
Stefan <stefan.joncic@united.group>
2022-05-24 16:20:04 +0200

    contact-us page, styling, form add new message

--- src/app/page/contact-us/contact-us.component.ts
+++ src/app/page/contact-us/contact-us.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { ApiService } from 'src/app/service/api.service';
 
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
-  styleUrls: ['./contact-us.component.scss']
+  styleUrls: ['./contact-us.component.scss'],
 })
 export class ContactUsComponent implements OnInit {
 
-  constructor() { }
+  contactForm = this.fb.group({
+    name: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    phone: ['', Validators.required],
+    message: ['', Validators.required],
+  });
 
-  ngOnInit(): void {
+  imgCAN: string = '../../../assets/images/contact/illustration-canada.svg';
+  imgAUS: string = '../../../assets/images/contact/illustration-australia.svg';
+  imgUK: string = '../../../assets/images/contact/illustration-united-kingdom.svg';
+
+  constructor(private service: ApiService, private fb: FormBuilder) {}
+
+  ngOnInit(): void {}
+
+  contacUs() {
+    if (this.contactForm.valid) {
+      this.service.addContactUs(this.contactForm.value).subscribe((res) => {
+        console.log(res);
+        this.contactForm.reset();
+      });
+    }
   }
-
 }
