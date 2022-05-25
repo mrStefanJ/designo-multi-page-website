bdb56ffe8ab4dda4ad2d42903bca924425a0461e
Stefan <stefan.joncic@united.group>
2022-05-25 12:42:01 +0200

    add new methond in service

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,10 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ContactUS } from '../model/contacUs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  private URL = 'http://localhost:3000';
 
   constructor(private service: HttpClient) { }
 
@@ -24,5 +26,9 @@ export class ApiService {
   }
   getDataGraphicDesign(){
     return this.service.get<any>("http://localhost:3000/graphicDesign");
+  }
+
+  addContactUs(contact: ContactUS) {
+    return this.service.post("http://localhost:3000/contact", contact);
   }
 }
